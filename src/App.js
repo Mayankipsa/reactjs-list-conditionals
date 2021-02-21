6798413b36ae42dfc010e54021052aced62eb8ae
Mayank <mayankipsa@gmail.com>
2021-02-21 19:55:43 +0530

    Created Components for Chars and Validation.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import Validation from "./Validation/Validation";
+import Char from "./Char/Char";
+import char from './Char/Char';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    userInput: ''
+  }
+
+  inputChangedHandler = (event) => {
+    this.setState(
+      {
+        userInput: event.target.value
+      }
+    );
+  }
+
+  deleteCharHandler = (index) => {
+    const text = this.state.userInput.split('');
+    text.splice(index, 1);
+    const updatedText = text.join('');
+    this.setState(
+      {
+        userInput: updatedText
+      }
+    );
+  }
+
+  render() {
+    const charList = this.state.userInput.split('').map((ch, index) => {
+      return <Char
+        character={ch}
+        key={index}
+        clicked={() => this.deleteCharHandler(index)}
+      ></Char>
+    });
+
+    return (
+      <div className="App">
+        <h1>Enter Text </h1>
+        <h3>Click on char below to delete from text</h3>
+        <input
+          type="text"
+          onChange={this.inputChangedHandler}
+          value={this.state.userInput}></input>
+        <p>{this.state.userInput}</p>
+        <Validation inputLength={this.state.userInput.length}></Validation>
+        {charList}
+      </div>
+    );
+  }
+
 }
 
 export default App;
